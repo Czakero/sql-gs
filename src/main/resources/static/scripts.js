5d86467337952cb416fd63d13340e273257863eb
Czakero <bartosz.pyrz96@gmail.com>
2019-02-27 23:21:51 +0100

    Deleted js script file, deleted reference for script and script function file in every html file

--- src/main/resources/static/scripts.js
+++
@@ -1,7 +0,0 @@
-function parseQuery() {
-    let queryValue = document.getElementById("query").value;
-    let regex = /[a-z]{3}/;
-    let isProperQuery = regex.test(queryValue);
-    let button = document.getElementById("submitButton");
-    button.disabled = !isProperQuery;
-}
