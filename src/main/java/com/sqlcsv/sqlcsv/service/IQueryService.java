f3410f2761360ada6f540cb5c69c94932464cef8
Czakero <bartosz.pyrz96@gmail.com>
2019-03-04 21:06:53 +0100

    Handle query now throws custom exception

--- src/main/java/com/sqlcsv/sqlcsv/service/IQueryService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/IQueryService.java
@@ -1,7 +1,8 @@
 package com.sqlcsv.sqlcsv.service;
 
+import com.sqlcsv.sqlcsv.controller.exception.ParseQueryException;
 import com.sqlcsv.sqlcsv.model.Table;
 
 public interface IQueryService {
-    Table handleQuery(String query);
+    Table handleQuery(String query) throws ParseQueryException;
 }
