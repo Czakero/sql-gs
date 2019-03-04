42a57dae163077e1332b13fa5ccade8d06085ca4
Czakero <bartosz.pyrz96@gmail.com>
2019-03-04 21:07:28 +0100

    Query service implements query service interface, main for testing

--- src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
@@ -1,13 +1,31 @@
 package com.sqlcsv.sqlcsv.service;
 
+import com.sqlcsv.sqlcsv.controller.exception.ParseQueryException;
 import com.sqlcsv.sqlcsv.model.Table;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class QueryService implements IQueryService {
-    @Override
-    public Table handleQuery(String query) {
 
+    private ISheetsService sheetsService;
+
+    @Autowired
+    public QueryService(ISheetsService sheetsService) {
+        this.sheetsService = sheetsService;
+    }
+
+    @Override
+    public Table handleQuery(String query) throws ParseQueryException {
+        String[] splittedQuery = query.split(" ");
+        System.out.println(Arrays.toString(splittedQuery));
         return null;
+    }
+
+    public static void main(String[] args) {
+        QueryService queryService = new QueryService(new SheetsService(new SheetsParser()));
+        queryService.handleQuery("SELECT * FROM dupa WHERE cipa like 'ciasna';");
     }
 }
