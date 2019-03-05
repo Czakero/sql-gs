76e043221b2a1af88fe30f3f8a3d2ce80ede4452
Czakero <bartosz.pyrz96@gmail.com>
2019-03-05 19:19:55 +0100

    New static final field with SQL keywords, handle query method now parses query to map for further usage

--- src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
@@ -5,11 +5,14 @@ import com.sqlcsv.sqlcsv.model.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.*;
 
 @Service
 public class QueryService implements IQueryService {
 
+    private static final List<String> KEYWORDS = new ArrayList<>(Arrays.asList("SELECT", "FROM", "WHERE", "LIMIT", "OFFSET"));
     private ISheetsService sheetsService;
 
     @Autowired
@@ -18,14 +21,33 @@ public class QueryService implements IQueryService {
     }
 
     @Override
-    public Table handleQuery(String query) throws ParseQueryException {
-        String[] splittedQuery = query.split(" ");
-        System.out.println(Arrays.toString(splittedQuery));
+    public Table handleQuery(String query, String spreadsheetId, String userId) throws ParseQueryException, IOException, GeneralSecurityException {
+        Map<String, List<String>> parsedQuery = mapQuery(query);
+        Table queriedSheet = sheetsService.getSheetFromSpreadsheet(spreadsheetId, parsedQuery.get("FROM").get(0), userId);
+        if (queriedSheet == null) {
+            throw new ParseQueryException("Provided sheet in FROM clause does not exist!");
+        }
+        System.out.println(parsedQuery.toString());
+        System.out.println(queriedSheet);
         return null;
     }
 
-    public static void main(String[] args) {
-        QueryService queryService = new QueryService(new SheetsService(new SheetsParser()));
-        queryService.handleQuery("SELECT * FROM dupa WHERE cipa like 'ciasna';");
+    private Map<String, List<String>> mapQuery(String query) {
+        String keyword = "";
+        Map<String, List<String>> result = new HashMap<>();
+        for (String word : query.split(" ")) {
+            if (KEYWORDS.contains(word.toUpperCase())) {
+                keyword = word;
+                result.put(keyword, new ArrayList<>());
+            } else {
+                result.get(keyword).add(word.replaceAll(",", "").replaceAll(";", ""));
+            }
+        }
+        try {
+            result.replace("WHERE", Collections.singletonList(String.join(",", result.get("WHERE")).replaceAll(",", " ")));
+        } catch (Exception e) {
+            System.out.println("No where clause");
+        }
+        return result;
     }
 }
