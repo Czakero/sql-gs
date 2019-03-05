212d3d9bb5a00447d15a25f0fb60ef668dd1cb5b
Czakero <bartosz.pyrz96@gmail.com>
2019-03-05 19:17:47 +0100

    Updated method signature to accept needed parameters

--- src/main/java/com/sqlcsv/sqlcsv/service/IQueryService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/IQueryService.java
@@ -3,6 +3,9 @@ package com.sqlcsv.sqlcsv.service;
 import com.sqlcsv.sqlcsv.controller.exception.ParseQueryException;
 import com.sqlcsv.sqlcsv.model.Table;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public interface IQueryService {
-    Table handleQuery(String query) throws ParseQueryException;
+    Table handleQuery(String query, String spreadsheetId, String userId) throws ParseQueryException, IOException, GeneralSecurityException;
 }
