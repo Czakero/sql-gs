a3e6159f0b77d9a866f0606b7f375c22a8e5d95a
Czakero <bartosz.pyrz96@gmail.com>
2019-03-05 19:18:11 +0100

    New method in interface, not needed but maybe in the future

--- src/main/java/com/sqlcsv/sqlcsv/service/ISheetsService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/ISheetsService.java
@@ -5,8 +5,10 @@ import com.sqlcsv.sqlcsv.model.Table;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.Map;
 
 public interface ISheetsService {
     Table getSheetFromSpreadsheet(String spreadsheetId, String sheetName, String userId) throws IOException, GeneralSecurityException;
     List<String> getSheetsNamesFromSpreadsheet(String spreadsheetId, String userId) throws IOException, GeneralSecurityException;
+    Map<String, String> getSheetsWithIds(String spreadsheetId, String userId) throws IOException, GeneralSecurityException;
 }
