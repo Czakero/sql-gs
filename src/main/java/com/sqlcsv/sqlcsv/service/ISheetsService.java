ddbf0dfbf8fe10d67fd6be0d1543ab9cafa84a9b
Czakero <bartosz.pyrz96@gmail.com>
2019-03-19 19:10:41 +0100

    Changed return type of method from table to double array

--- src/main/java/com/sqlcsv/sqlcsv/service/ISheetsService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/ISheetsService.java
@@ -1,14 +1,12 @@
 package com.sqlcsv.sqlcsv.service;
 
-import com.sqlcsv.sqlcsv.model.Table;
-
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.Map;
 
 public interface ISheetsService {
-    Table getSheetFromSpreadsheet(String spreadsheetId, String sheetName, String userId) throws IOException, GeneralSecurityException;
+    String[][] getSheetFromSpreadsheet(String spreadsheetId, String sheetName, String userId) throws IOException, GeneralSecurityException;
     List<String> getSheetsNamesFromSpreadsheet(String spreadsheetId, String userId) throws IOException, GeneralSecurityException;
     Map<String, String> getSheetsWithIds(String spreadsheetId, String userId) throws IOException, GeneralSecurityException;
 }
