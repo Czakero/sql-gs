316cff0fb96dd76fc8bef8706e8cb88c6fe0cbee
Czakero <bartosz.pyrz96@gmail.com>
2019-03-19 19:12:36 +0100

    Big refactor, moved logic to their own classes to make it all more readable and maintainable as proposed in SRP

--- src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/QueryService.java
@@ -1,53 +1,37 @@
 package com.sqlcsv.sqlcsv.service;
 
 import com.sqlcsv.sqlcsv.controller.exception.ParseQueryException;
-import com.sqlcsv.sqlcsv.model.Table;
+import com.sqlcsv.sqlcsv.model.SQLKeywords;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class QueryService implements IQueryService {
 
-    private static final List<String> KEYWORDS = new ArrayList<>(Arrays.asList("SELECT", "FROM", "WHERE", "LIMIT", "OFFSET"));
     private ISheetsService sheetsService;
+    private IQueryChecker queryChecker;
+    private IQueryMapper queryMapper;
+    private IQueryExecutor queryExecutor;
 
     @Autowired
-    public QueryService(ISheetsService sheetsService) {
+    public QueryService(ISheetsService sheetsService, IQueryChecker queryChecker, IQueryMapper queryMapper, IQueryExecutor queryExecutor) {
         this.sheetsService = sheetsService;
+        this.queryChecker = queryChecker;
+        this.queryMapper = queryMapper;
+        this.queryExecutor = queryExecutor;
     }
 
     @Override
-    public Table handleQuery(String query, String spreadsheetId, String userId) throws ParseQueryException, IOException, GeneralSecurityException {
-        Map<String, List<String>> parsedQuery = mapQuery(query);
-        Table queriedSheet = sheetsService.getSheetFromSpreadsheet(spreadsheetId, parsedQuery.get("FROM").get(0), userId);
-        if (queriedSheet == null) {
-            throw new ParseQueryException("Provided sheet in FROM clause does not exist!");
-        }
-        System.out.println(parsedQuery.toString());
-        System.out.println(queriedSheet);
-        return null;
-    }
-
-    private Map<String, List<String>> mapQuery(String query) {
-        String keyword = "";
-        Map<String, List<String>> result = new HashMap<>();
-        for (String word : query.split(" ")) {
-            if (KEYWORDS.contains(word.toUpperCase())) {
-                keyword = word;
-                result.put(keyword, new ArrayList<>());
-            } else {
-                result.get(keyword).add(word.replaceAll(",", "").replaceAll(";", ""));
-            }
-        }
-        try {
-            result.replace("WHERE", Collections.singletonList(String.join(",", result.get("WHERE")).replaceAll(",", " ")));
-        } catch (Exception e) {
-            System.out.println("No where clause");
-        }
-        return result;
+    public String[][] handleQuery(String query, String spreadsheetId, String userId) throws ParseQueryException, IOException, GeneralSecurityException {
+        queryChecker.checkQuery(query);
+        Map<SQLKeywords, List<String>> parsedQuery = queryMapper.mapQuery(query);
+        String[][] queriedSheet = sheetsService.getSheetFromSpreadsheet(spreadsheetId, parsedQuery.get(SQLKeywords.FROM).get(0), userId);
+        parsedQuery.remove(SQLKeywords.FROM);
+        return queryExecutor.executeQuery(queriedSheet, parsedQuery);
     }
 }
