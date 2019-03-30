50cf08ecf447de2789fee039906708299a4b090c
Czakero <bartosz.pyrz96@gmail.com>
2019-03-30 15:19:00 +0100

    Moved to googleservice package, little code refactor and new static final field removing magic

--- src/main/java/com/sqlcsv/sqlcsv/service/googleservice/DriveService.java
+++ src/main/java/com/sqlcsv/sqlcsv/service/googleservice/DriveService.java
@@ -1,10 +1,11 @@
-package com.sqlcsv.sqlcsv.service;
+package com.sqlcsv.sqlcsv.service.googleservice;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 import com.sqlcsv.sqlcsv.google.GoogleAuthorizationFlow;
-import com.sqlcsv.sqlcsv.google.Services;
+import com.sqlcsv.sqlcsv.enums.ServicesEnum;
+import com.sqlcsv.sqlcsv.interfaces.IDriveService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,14 +16,14 @@ import java.util.stream.Collectors;
 
 @Service
 public class DriveService implements IDriveService {
+    private static final String SPREADSHEET = "application/vnd.google-apps.spreadsheet";
     public Map<String, String> getAllSpreadsheets(String userId) throws IOException, GeneralSecurityException {
-        Drive userDrive = (Drive) GoogleAuthorizationFlow.getService(Services.DRIVE, userId);
-        Drive.Files.List request = Objects.requireNonNull(userDrive).files().list();
-        FileList files = request.execute();
+        Drive userDrive = (Drive) GoogleAuthorizationFlow.getService(ServicesEnum.DRIVE, userId);
+        FileList files = Objects.requireNonNull(userDrive).files().list().execute();
 
         return files.getFiles()
                 .stream()
-                .filter(s -> s.getMimeType().equals("application/vnd.google-apps.spreadsheet"))
+                .filter(s -> s.getMimeType().equals(SPREADSHEET))
                 .collect(Collectors.toMap(File::getName, File::getId));
     }
 }
